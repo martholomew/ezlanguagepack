882703253e326aca3976eae940131ea4cb61b1b0
matttttt <18152455+martholomew@users.noreply.github.com>
2019-12-15 18:59:31 -0700

    Fixed some junk, hopefully?

--- make_keyboards.py
+++ make_keyboards.py
@@ -10,9 +10,6 @@ from pprint import pprint
 import xml.etree.ElementTree as et
 
 
-#popupKeyboard="@xml/popup_16keys_abc"
-
-
 with open("lang_acc.json", "r") as f:
     langs = json.load(f)
 
@@ -21,7 +18,6 @@ pretty_language = language.title()
 language_short = sys.argv[2]
 
 assert language in langs.keys()
-print("noice")
 
 accents = langs[language]
 
@@ -43,7 +39,7 @@ for keyboard in keyboards:
                 if key.attrib[codes] == alpha:
                     key.attrib[popup] = f"@xml/popup_{alpha}"
 
-        new_keyboard = language + "/pack/src/main/res/xml/" + language + "_" + keyboard_type + ".xml"
+        new_keyboard = f"{language}/pack/src/main/res/xml/{language}_{keyboard_type}.xml"
         tree = et.ElementTree(xml)
         tree.write(new_keyboard, xml_declaration=True, encoding='utf-8')
         with open(new_keyboard, "r+") as s:
@@ -54,19 +50,19 @@ for keyboard in keyboards:
     
     lang_short = language_short
     md5 = hex(random.getrandbits(128))[2:]
-    md5 = md5[:8] + "-" + md5[8:12] + "-" + md5[12:16] + "-" + md5[16:20] + "-" + md5[20:]
+    md5 = f"{md5[:8]}-{md5[8:12]}-{md5[12:16]}-{md5[16:20]}-{md5[20:]}"
     keyboard_xml = et.SubElement(keyboards_xml, 'Keyboard')
-    keyboard_xml.set("nameResId", pretty_language + " " + keyboard_type_pretty)
-    keyboard_xml.set("iconResId", "@drawable/ic_status_" + language)
-    keyboard_xml.set("layoutResId", "@xml/" + language + "_" + keyboard_type_pretty)
+    keyboard_xml.set("nameResId", f"{pretty_language} {keyboard_type_pretty}")
+    keyboard_xml.set("iconResId", f"@drawable/ic_status_{language}")
+    keyboard_xml.set("layoutResId", f"@xml/{language}_{keyboard_type}")
     keyboard_xml.set("id", md5)
     keyboard_xml.set("defaultDictionaryLocale", lang_short)
-    keyboard_xml.set("description", pretty_language + " " + keyboard_type_pretty)
+    keyboard_xml.set("description", f"{pretty_language} {keyboard_type_pretty}")
     keyboard_xml.set("index", str(index))
 
     index += 1
 
-new_keyboards = language + "/pack/src/main/res/xml/" + language + "_" + "keyboards.xml"
+new_keyboards = f"{language}/pack/src/main/res/xml/{language}_keyboards.xml"
 tree = et.ElementTree(keyboards_xml)
 tree.write(new_keyboards, xml_declaration=True, encoding='utf-8')
 
@@ -76,7 +72,7 @@ for alpha in accents:
     row = et.SubElement(xml, 'Row')
     for accent in accents[alpha]:
         et.SubElement(row, 'Key').set("android:codes", accent)
-    new_popup = language + "/pack/src/main/res/xml/" + "popup_" + alpha + ".xml"
+    new_popup = language + f"/pack/src/main/res/xml/{language}_popup_{alpha}.xml"
     tree = et.ElementTree(xml)
     tree.write(new_popup, xml_declaration=True, encoding='utf-8')
 
@@ -91,6 +87,6 @@ dicitonary_xml.set("id", md5)
 dicitonary_xml.set("locale", language_short)
 dicitonary_xml.set("nameResId", f"{pretty_language} Dictionary")
 dicitonary_xml.set("type", "binary_resource")
-new_dictionaries = language + "/pack/src/main/res/xml/" + language + "_" + "dictionaries.xml"
+new_dictionaries = f"{language}/pack/src/main/res/xml/{language}_dictionaries.xml"
 tree = et.ElementTree(dictionaries_xml)
 tree.write(new_dictionaries, xml_declaration=True, encoding='utf-8')
